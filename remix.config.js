3ea5e72a678b3efd7a63e784ac05a9b0bf2feb02
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-11-22 09:12:48 -0300

    Add `remix-i18next` to dependencies to bundle
    To avoid the error:
    
    > SyntaxError: Named export 'RemixI18Next' not found.
    > The requested module 'remix-i18next' is a CommonJS module,
    > ...
    
    Which is caused by a bug in the way the library is exposing this hybrid
    package. See https://github.com/sergiodxa/remix-i18next/issues/143

--- remix.config.js
+++ remix.config.js
@@ -1,6 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
   ignoredRouteFiles: ["**/.*"],
+  serverDependenciesToBundle: [
+    "remix-i18next",
+  ],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
